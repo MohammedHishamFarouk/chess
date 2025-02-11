4ed90ce5e3e6742f8f89a49fbaa5ff662691b655
mohamed hisham <mohamedhis36@gmail.com>
2025-02-11 18:18:43 +0200

    added pieces assets and the pieces dart file in addition to adding the svg package

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,10 +1,17 @@
+import 'package:chess/objects/pieces.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: Center(
+        child: SvgPicture.asset(Pieces.wBishop),
+      ),
+    );
   }
 }
