3ef900d4f022f3c0bfa5dd0a1b4d33c7fef1be25
mohamed hisham <mohamedhis36@gmail.com>
2025-02-19 21:28:42 +0200

    did some cleaning

--- lib/helper/functions.dart
+++ lib/helper/functions.dart
@@ -1,6 +1,6 @@
 bool isWhite(int index) {
-  int x = index ~/ 8;
-  int y = index % 8;
+  int x = index ~/ 8; // find the row
+  int y = index % 8; // find the column
   bool isWhite = (x + y) % 2 == 0;
   return isWhite;
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -7,6 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
+      body: Center(),
     );
   }
 }
