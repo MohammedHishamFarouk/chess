f683a842f04d133a4010568bc921df091cf003a4
mohamed hisham <mohamedhis36@gmail.com>
2025-02-26 12:33:22 +0200

    added a gestureDetector to teh square and made the square color change when selected

--- lib/components/square.dart
+++ lib/components/square.dart
@@ -3,20 +3,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Square extends StatelessWidget {
-  const Square({super.key, required this.isWhite, required this.piece});
+  const Square({
+    super.key,
+    required this.isWhite,
+    required this.piece,
+    required this.isSelected,
+    this.onTap,
+    required this.isValidMoves,
+  });
   final bool isWhite;
   final ChessPiece? piece;
+  final bool isSelected;
+  final bool isValidMoves;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: isWhite ? Colors.grey : Colors.grey.shade900,
-      child: piece != null
-          ? SvgPicture.asset(
-              piece!.imagePath,
-              color: piece!.isWhite ? Colors.white : null,
-            )
-          : null,
+    Color? squareColor;
+    //if selected,square color will be green
+    if (isSelected) {
+      squareColor = Colors.green;
+    } else if (isValidMoves) {
+      squareColor = Colors.lightGreen;
+    } else {
+      squareColor = isWhite ? Colors.grey : Colors.grey.shade900;
+    }
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: squareColor,
+        child: piece != null
+            ? SvgPicture.asset(
+                piece!.imagePath,
+                color: piece!.isWhite ? Colors.white : null,
+              )
+            : null,
+      ),
     );
   }
 }
